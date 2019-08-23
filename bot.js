64145bafeb03a802baeb279ea972d77d5794dbb8
Dowzie <54423317+Dowzie@users.noreply.github.com>
2019-08-23 03:21:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,8 @@ client.on('message', message => {
 
     if (message.content === '!tipeee') {
 
-       message.reply('Go me support sur Tipeee si tu aime mon job, plein de trucs cool en contrepartie ;) https://fr.tipeee.com/dowzie');
+       message.reply('Go me support sur Tipeee si tu aime mon job, plein de trucs cool en contrepartie ;) https://fr.tipeee.com/dowzie')
+     
            if (message.content === '!chouchou') {
 
        message.reply('N'hésite pas à follow Chouchou sur les réseaux : 
@@ -26,7 +27,8 @@ Twitter : https://twitter.com/ChouchouGeekArt
 Instagram : https://www.instagram.com/chouchougeekart
 Twitch : https://www.twitch.tv/chouchougeekart
 Site internet : https://chouchougeekart.blogspot.com/
-Youtube : https://www.youtube.com/channel/UC4YWjAguofYoh29b6rBTwCw/             ');
+Youtube : https://www.youtube.com/channel/UC4YWjAguofYoh29b6rBTwCw/             ')
+                     
       if (message.content === '!dowzie') {
 
        message.reply('N'hésite pas à follow Dowzie sur les réseaux : 
@@ -34,7 +36,7 @@ Facebook : https://www.facebook.com/DowzieCosplay
 Twitter : https://twitter.com/dovvzie
 Instagram :  https://www.instagram.com/dovvzie
 Twitch: https://www.twitch.tv/dovvzie
-Tipeee: https://fr.tipeee.com/dowzie             ');
+Tipeee: https://fr.tipeee.com/dowzie             ')
     
       if (message.content === '!print') {
 
