d83f62d173cfb00b59eb9116a40ed210655f8ff4
Dowzie <54423317+Dowzie@users.noreply.github.com>
2019-08-23 03:38:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,42 +10,60 @@ client.on('ready', () => {
 
 });
 
- 
+ var generalChannel = client.channels.get("général") // Replace with known channel ID
+   // Provide a URL to a file
+    const webAttachment = new Discord.Attachment('https://giphy.com/gifs/kaamelott-XknChYwfPnp04')
+    generalChannel.send(webAttachment)
+})
 
-client.on('message', message => {
- 
-
-    if (message.content === '!tipeee') {
-
-       message.reply('Go me support sur Tipeee si tu aime mon job, plein de trucs cool en contrepartie ;) https://fr.tipeee.com/dowzie')
-     
-           if (message.content === '!chouchou') {
-
-       message.reply('N'hésite pas à follow Chouchou sur les réseaux : 
-Facebook : https://www.facebook.com/Chouchougeekart
-Twitter : https://twitter.com/ChouchouGeekArt
-Instagram : https://www.instagram.com/chouchougeekart
-Twitch : https://www.twitch.tv/chouchougeekart
-Site internet : https://chouchougeekart.blogspot.com/
-Youtube : https://www.youtube.com/channel/UC4YWjAguofYoh29b6rBTwCw/             ')
-                     
-      if (message.content === '!dowzie') {
-
-       message.reply('N'hésite pas à follow Dowzie sur les réseaux : 
-Facebook : https://www.facebook.com/DowzieCosplay
-Twitter : https://twitter.com/dovvzie
-Instagram :  https://www.instagram.com/dovvzie
-Twitch: https://www.twitch.tv/dovvzie
-Tipeee: https://fr.tipeee.com/dowzie             ')
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+        return
+    }
     
-      if (message.content === '!print') {
+    if (receivedMessage.content.startsWith("!")) {
+        processCommand(receivedMessage)
+    }
+})
 
-       message.reply('Prints cosplay disponibles : https://forms.gle/KcaZNGCHivGCw4x89');
-      
-     
-    
+function processCommand(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
 
-       }
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments) // There may not be any arguments
+
+    if (primaryCommand == "help") {
+        helpCommand(arguments, receivedMessage)
+    } else if (primaryCommand == "multiply") {
+        multiplyCommand(arguments, receivedMessage)
+    } else {
+        receivedMessage.channel.send("I don't understand the command. Try `!help` or `!multiply`")
+    }
+}
+
+function helpCommand(arguments, receivedMessage) {
+    if (arguments.length > 0) {
+        receivedMessage.channel.send("It looks like you might need help with " + arguments)
+    } else {
+        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+    }
+}
+
+function multiplyCommand(arguments, receivedMessage) {
+    if (arguments.length < 2) {
+        receivedMessage.channel.send("Not enough values to multiply. Try `!multiply 2 4 10` or `!multiply 5.2 7`")
+        return
+    }
+    let product = 1 
+    arguments.forEach((value) => {
+        product = product * parseFloat(value)
+    })
+    receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
+}
+
 
 });
 
