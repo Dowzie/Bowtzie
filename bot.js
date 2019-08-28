162cbc2321dd5d319285fb70143dfaa6aab21b08
Dowzie <54423317+Dowzie@users.noreply.github.com>
2019-08-28 23:29:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,10 @@ client.on('message', message => {
     if (message.content === '!tipeee'){
        message.reply('Go me support sur Tipeee si tu aime mon job, plein de trucs cool en contrepartie :wink: \nhttps://fr.tipeee.com/dowzie');
     }
+  if (message.content === '!chouchou'){
+       message.reply('Go follow pour ne rien rater ! \n:dagger:  Facebook : https://www.facebook.com/Chouchougeekart \n
+:dagger:  Twitter : https://twitter.com/ChouchouGeekArt \n:dagger:  Instagram : https://www.instagram.com/chouchougeekart \n:dagger:  Twitch : https://www.twitch.tv/chouchougeekart \n:dagger:  Site internet : https://chouchougeekart.blogspot.com \n:dagger:  Youtube : https://www.youtube.com/channel/UC4YWjAguofYoh29b6rBTwCw/featured :wink: \nhttps://fr.tipeee.com/dowzie');
+    }
     if (message.content.toLowerCase().includes('prout', 0) && !(message.author.bot)){
         message.reply('C\'est toi le prout !');
     }
