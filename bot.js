8d06ebaa6e9a1be9d6ad2783073c08d53b95d5cb
Geoffrey VAN PALM <gvanpalm@gmail.com>
2019-08-30 11:39:39 +0200

    Pizza Ananas Command + Help Command

--- bot.js
+++ bot.js
@@ -12,28 +12,48 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-
+ if(!(message.author.bot)){
+    if (message.content === '!help'){
+       message.reply('Voici les differentes commandes disponibles : \n \
+!ping \n \
+!tipeee \n \
+!chouchou \n \
+!dowzie \n \
+et d\'autres plus secrètes :wink:');
+    }
+ 
+    // Fun Commands
     if (message.content === '!ping') {
        message.reply('pong');
     }
-    if (message.content === '!tipeee'){
-       message.reply('Go me support sur Tipeee si tu aime mon job, plein de trucs cool en contrepartie :wink: \nhttps://fr.tipeee.com/dowzie');
-    }
-  if (message.content === '!chouchou'){
-       message.reply('Go follow pour ne rien rater ! \n:dagger:  Facebook : <https://www.facebook.com/Chouchougeekart> \n \
-:dagger:  Twitter : <https://twitter.com/ChouchouGeekArt> \n:dagger:  Instagram : <https://www.instagram.com/chouchougeekart> \n:dagger:  Twitch : <https://www.twitch.tv/chouchougeekart> \n:dagger:  Site internet : <https://chouchougeekart.blogspot.com> \n:dagger:  Youtube : <https://www.youtube.com/channel/UC4YWjAguofYoh29b6rBTwCw/featured>');
-    }
-   if (message.content.toLowerCase().includes('dovvzie', 0) && !(message.author.bot)){
+    if (message.content.toLowerCase().includes('dovvzie', 0) && !(message.author.bot)){
         message.channel.send('<@194524212134674432> c\'est un gros caca !');
-           message.channel.send('<@253491625328771073> AUSSI !');
+        message.channel.send('<@253491625328771073> AUSSI !');
     }
-  if (message.content === '!dowzie'){
-       message.reply('Go follow pour ne rien rater ! \n:pushpin:  Facebook : <https://www.facebook.com/DowzieCosplay> \n \
-:pushpin:  Twitter : <https://twitter.com/dovvzie> \n:pushpin:  Instagram : <https://www.instagram.com/dovvzie> \n:pushpin:  Twitch : <https://www.twitch.tv/dovvzie> \n:pushpin:  Tipeee : <https://fr.tipeee.com/dowzie>');
-    }
+  
     if (message.content.toLowerCase().includes('prout', 0) && !(message.author.bot)){
         message.reply('C\'est toi le prout !');
     }
+    if (message.content.toLowerCase().includes('pizza', 0) && message.content.toLowerCase().includes('ananas') && !(message.author.bot)){
+        message.channel.send('J\'ai entendu Pizza et Ananas dans la même phrase, j\'espere que vous ne mangez pas ca ! \n https://tenor.com/view/ew-disgust-gif-3671501');
+    }
+ 
+    // Info Commands
+ 
+    if (message.content === '!tipeee'){
+       message.reply('Go me support sur Tipeee si tu aime mon job, plein de trucs cool en contrepartie :wink: \nhttps://fr.tipeee.com/dowzie');
+    }
+    if (message.content === '!chouchou'){
+       message.reply('Go follow pour ne rien rater ! \n:dagger:  Facebook : <https://www.facebook.com/Chouchougeekart> \n \
+:dagger:  Twitter : <https://twitter.com/ChouchouGeekArt> \n:dagger:  Instagram : <https://www.instagram.com/chouchougeekart> \n:dagger:  Twitch : <https://www.twitch.tv/chouchougeekart> \n:dagger:  Site internet : <https://chouchougeekart.blogspot.com> \n:dagger:  Youtube : <https://www.youtube.com/channel/UC4YWjAguofYoh29b6rBTwCw/featured>');
+    }
+    if (message.content === '!dowzie'){
+       message.reply('Go follow pour ne rien rater ! \n:pushpin:  Facebook : <https://www.facebook.com/DowzieCosplay> \n \
+:pushpin:  Twitter : <https://twitter.com/dovvzie> \n:pushpin:  Instagram : <https://www.instagram.com/dovvzie> \n:pushpin:  Twitch : <https://www.twitch.tv/dovvzie> \n:pushpin:  Tipeee : <https://fr.tipeee.com/dowzie>');
+    }
+ }
+ 
+
 });
 
  
