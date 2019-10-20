4781c6772c45864973946fbc19e4984d9a2a66aa
Geoffrey VAN PALM <gvanpalm@gmail.com>
2019-10-20 21:09:52 +0200

    Mute Live Update

--- bot.js
+++ bot.js
@@ -99,11 +99,11 @@ client.on('message', message => {
 
 });
 
-client.on('presenceUpdate', (oldMember, newMember) => {
+/*client.on('presenceUpdate', (oldMember, newMember) => {
     if (newMember.presence.game && newMember.presence.game.streaming && newMember.id === '194524212134674432'){
 	    client.channels.get('618917882151174165').send(':dovvziBOOP: Test LIVE') ;
     }
-});
+});*/
 
  
 
