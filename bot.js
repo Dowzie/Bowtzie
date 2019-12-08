6d259442be32e60dc1ee53d51d03d6628c85ce70
Geoffrey VAN PALM <gvanpalm@gmail.com>
2019-12-08 12:48:08 +0100

    test stream start

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ client.on('message', message => {
 	 
     // Test Commands
 	if (message.content === '!testStream') {
-		StreamInfo = sendCUrlRequest('getStreamInfo', 'ZeratoR');
+		StreamInfo = sendCUrlRequest('getStreamInfo', 'ponce');
 	}
 
  
